7f0420e40b87b81e39393d41132f1718d11c39c8
LyghtCode <xikago.musica@gmail.com>
2023-02-02 23:03:22 -0500

    fix type

--- components/SaleInfo/SaleInfo.tsx
+++ components/SaleInfo/SaleInfo.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { NFT } from "@thirdweb-dev/sdk";
 
-type Props = {};
+type Props = {
+  nft: NFT;
+};
 
-export default function SaleInfo({}: Props) {
+export default function SaleInfo({ nft }: Props) {
   return <div>SaleInfo</div>;
 }
