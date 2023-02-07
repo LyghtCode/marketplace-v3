fb2800da22d292221f355b189f6a465e6b9fc71c
LyghtCode <xikago.musica@gmail.com>
2023-02-07 17:02:32 -0500

    stylesss

--- pages/about.tsx
+++ pages/about.tsx
@@ -28,18 +28,18 @@ const About: NextPage = () => {
            
           </div>
           <div className={styles.heroBodyContainer}>
-             <Image
+         
+            <div className={styles.heroBody}>
+            <Image
               src="/logo-main.png"
-              width={270}
-              height={270}
+              width={280}
+              height={250}
               alt="Restore Coral NFT Marketplace"
               quality={100}
               className={styles.heroAsset}
             />
-            <div className={styles.heroBody}>
               <h1 className={styles.heroTitle}>
                 QUIÉNES SOMOS
-                
               </h1>
               <p className={styles.heroSubtitle}>
               Somos una organización trabajando para la restauración de los sistemas arrecifales en <b>Mexico</b>, empezando por el <i>Sistema Arrecifal Mesoamericano.</i> A través de la innovación social, el arte y el uso de tecnologias emergentes buscamos sensibilizar al público acerca de la importancia de la protección de los arrecifes de coral. Buscamos impulsar un modelo de <b>sustentabilidad</b> que proteja el medio marino, al mismo tiempo que permita desarrollar estrategias productivas responsables socialmente, las cuales involucren a las comunidades, como el ecoturismo.
