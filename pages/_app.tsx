8c837d8a562987e32aa781f327c672369fec0640
LyghtCode <xikago.musica@gmail.com>
2023-02-26 10:10:15 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const address = useAddress();
+  console.log(address);
 
   return (
     <ThirdwebProvider desiredChainId={NETWORK_ID}>
@@ -23,85 +24,86 @@ function MyApp({ Component, pageProps }: AppProps) {
         showOnShallow={true}
       />
       <NextUIProvider>
-      <Navbar isBordered variant="floating">
-        <Navbar.Toggle showIn="xs" />
-        <Navbar.Brand
-          css={{
-            "@xs": {
-              w: "12%",
-            },
-          }}
-        
-        >
-          <Link href="/">
-          <Image
-              src="/simbolo.png"
-              width={48}
-              height={48}
-              alt="Restore coral NFT Marketplace"
-            />
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Content
-          enableCursorHighlight
-          activeColor="secondary"
-          hideIn="xs"
-          variant="highlight-rounded"
-        >
-          <Navbar.Link href="/buy">Adopt-a-Coral</Navbar.Link>
-          <Navbar.Link href="/about">
-            About
-          </Navbar.Link>
-          <Navbar.Link href="/arrecifes">Reefs</Navbar.Link>
-          <Navbar.Link href="/historia">Our Story</Navbar.Link>
-        </Navbar.Content>
-        <Navbar.Content
-          css={{
-            "@xs": {
-              w: "12%",
-              jc: "flex-end",
-            },
-          }}
-        >
-          <Dropdown placement="bottom-right">
-            <Navbar.Item>
-              <Dropdown.Trigger>
-              {/* <ConnectWallet />
-               */}
-               <Image
-                // className={styles.profileImage}
-                src="/user-icon.png"
-                width={42}
-                height={42}
-                alt="Profile"
+        <Navbar isBordered variant="floating">
+          <Navbar.Toggle showIn="xs" />
+          <Navbar.Brand
+            css={{
+              "@xs": {
+                w: "12%",
+              },
+            }}
+
+          >
+            <Link href="/">
+              <Image
+                src="/simbolo.png"
+                width={48}
+                height={48}
+                alt="Restore coral NFT Marketplace"
               />
-              </Dropdown.Trigger>
-            </Navbar.Item>
-            <Dropdown.Menu
-              aria-label="User menu actions"
-              color="secondary"
-              onAction={(actionKey) => console.log({ actionKey })}
-            >
-              <Dropdown.Item key="profile" css={{ height: "$18" }}>
-                <Text b color="inherit" css={{ d: "flex" }}>
-                  Signed in as
-                </Text>
-                <Text b color="inherit" css={{ d: "flex" }}>
-                  {address}
-                </Text>
-              </Dropdown.Item>
-              <Dropdown.Item key="settings" >
-                <Link color="inherit" href={`/profile/${address}`}>
-                My Profile
-                </Link>
-              </Dropdown.Item>
-              <Dropdown.Item key="connect" withDivider css={{ height: "$18" }}>
-                <ConnectWallet />
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Navbar.Content>
-        <Navbar.Collapse>
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Content
+            enableCursorHighlight
+            activeColor="secondary"
+            hideIn="xs"
+            variant="highlight-rounded"
+          >
+            <Navbar.Link href="/buy">Adopt-a-Coral</Navbar.Link>
+            <Navbar.Link href="/about">
+              About
+            </Navbar.Link>
+            <Navbar.Link href="/arrecifes">Reefs</Navbar.Link>
+            <Navbar.Link href="/historia">Our Story</Navbar.Link>
+          </Navbar.Content>
+          <Navbar.Content
+            css={{
+              "@xs": {
+                w: "12%",
+                jc: "flex-end",
+              },
+            }}
+          >
+            <Dropdown placement="bottom-right">
+              <Navbar.Item>
+                <Dropdown.Trigger>
+                  {/* <ConnectWallet />
+               */}
+                  <Image
+                    // className={styles.profileImage}
+                    src="/user-icon.png"
+                    width={42}
+                    height={42}
+                    alt="Profile"
+                  />
+                </Dropdown.Trigger>
+              </Navbar.Item>
+              <Dropdown.Menu
+                aria-label="User menu actions"
+                color="secondary"
+                onAction={(actionKey) => console.log({ actionKey })}
+              >
+                <Dropdown.Item key="profile" css={{ height: "$18" }}>
+                  <Text b color="inherit" css={{ d: "flex" }}>
+                    Signed in as
+                  </Text>
+                  {address && (<Text b color="inherit" css={{ d: "flex" }}>
+                    {address as string}
+                  </Text>)}
+
+                </Dropdown.Item>
+                <Dropdown.Item key="settings" >
+                  {address && (<Link color="inherit" href={"/profile/" + `${address as string}`}>
+                  </Link>
+                  )}
+                </Dropdown.Item>
+                <Dropdown.Item key="connect" withDivider css={{ height: "$18" }}>
+                  <ConnectWallet />
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Navbar.Content>
+          <Navbar.Collapse>
             <Navbar.CollapseItem
               key={1}
               activeColor="secondary"
@@ -127,7 +129,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                 }}
                 href="/about"
               >
-              About
+                About
               </Link>
             </Navbar.CollapseItem>
             <Navbar.CollapseItem
@@ -158,9 +160,9 @@ function MyApp({ Component, pageProps }: AppProps) {
                 Our Story
               </Link>
             </Navbar.CollapseItem>
-        </Navbar.Collapse>
-      </Navbar>
-      <Component {...pageProps} />
+          </Navbar.Collapse>
+        </Navbar>
+        <Component {...pageProps} />
       </NextUIProvider>
     </ThirdwebProvider>
   );
