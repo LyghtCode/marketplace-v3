ab6c6bcc62cc32e04da37ef07e35e033494b9782
LyghtCode <xikago.musica@gmail.com>
2023-04-19 00:39:00 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const Home: NextPage = () => {
             >
             </Player>
             <div className={styles.heroBackgroundInner}>
-
+            
             </div>
           </div>
           <div className={styles.heroAssetFrame}>
