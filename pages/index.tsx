20f729fc902122f79d592d621d85031e205b2316
Lyght Code <taino@olmec.ai>
2023-07-20 12:03:53 -0600

    video
    add video

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import ReactPlayer from 'react-player'
 
 /**
  * Landing page with a simple gradient background and a hero asset.
@@ -13,15 +14,18 @@ const Home: NextPage = () => {
       <div className={styles.content}>
         <div className={styles.hero}>
           <div className={styles.heroBackground}>
-            {/* <video autoPlay muted loop id="myVideo" src="https://www.canva.com/design/DAFR9o9eXJE/qmBqeG9mGlW-tg8yKgw9Qg/watch?utm_content=DAFR9o9eXJE&utm_campaign=share_your_design&utm_medium=link&utm_source=shareyourdesignpanel">
-            </video> */}
-            <Player
+          <ReactPlayer url='https://www.youtube.com/watch?v=AYRgbwfjc7w' 
+            playing={true}
+            loop={true}
+            width={'100vw'}
+            height={'100vh'}
+            />
+            {/* <Player
               autoplay
               loop
               src="https://assets10.lottiefiles.com/packages/lf20_nzypsmyf.json"
-            // style={{ height: '100vh', width: '100vw' }}
             >
-            </Player>
+            </Player> */}
             <div className={styles.heroBackgroundInner}>
             
             </div>
@@ -30,6 +34,7 @@ const Home: NextPage = () => {
             {/* boop */}
           </div>
           <div >
+
             <Image
               src="/logo-main.png"
               width={330}
@@ -39,15 +44,15 @@ const Home: NextPage = () => {
               className={styles.heroAsset}
             />
             <div className={styles.heroBody}>
+     
+            <br/>
               <h1 className={styles.heroTitle}>
                 Our Mission
               </h1>
               <p className={styles.heroSubtitle}>
                 Regenerating Coastal & Marine Ecosystems, while reducing poverty and protecting ancestral wisdom with a combination of art, technology & social outreach programs.
               </p>
-              {/* <p className={styles.heroSubtitle}>
-              Implementamos innovación social, tecnologías emergentes y conocimientos tradicionales a favor de los océanos.
-              </p> */}
+              <video width={100} height={50} autoPlay muted loop id="myVideo" src="https://www.youtube.com/watch?v=AYRgbwfjc7w" />
             </div>
           </div>
         </div>
